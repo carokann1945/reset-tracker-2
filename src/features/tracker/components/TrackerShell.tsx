81152219d5fe44f13bdd47a26cee83e6ffcc855a
carokann1945 <carokann1945@gmail.com>
2026-03-23 15:02:20 +0900

    feat: TrackerShell에 motion 적용

--- src/features/tracker/components/TrackerShell.tsx
+++ src/features/tracker/components/TrackerShell.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { motion } from 'motion/react';
 import { cn } from '@/lib/utils';
 import { useResponsiveSidebar } from '../hooks/useResponsiveSidebar';
 import { useSidebarStore } from '../model/sidebarStore';
 import Header from './Header';
 import Sidebar from './sidebar/Sidebar';
 import TaskList from './task/TaskList';
+
+const variants = {
+  enter: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 export default function TrackerShell() {
   useResponsiveSidebar();
@@ -15,26 +25,33 @@ export default function TrackerShell() {
   if (!isMounted) return null;
 
   return (
-    <div className={cn('w-full min-h-dvh', 'flex')}>
-      <Sidebar />
-      {isOpen && (
+    <motion.div
+      key="result"
+      variants={variants}
+      initial="enter"
+      animate="visible"
+      transition={{ duration: 0.2, ease: 'easeInOut' }}>
+      <div className={cn('w-full min-h-dvh', 'flex')}>
+        <Sidebar />
+        {isOpen && (
+          <div
+            className="md:hidden fixed inset-0 bg-black/20 backdrop-blur-[2px] z-15 transition-opacity"
+            onClick={() => setIsOpen(false)}
+          />
+        )}
         <div
-          className="md:hidden fixed inset-0 bg-black/20 backdrop-blur-[2px] z-15 transition-opacity"
-          onClick={() => setIsOpen(false)}
-        />
-      )}
-      <div
-        className={cn(
-          'flex-1 h-full',
-          'flex flex-col',
-          'transition-all duration-300',
-          isOpen ? 'md:ml-[300px]' : 'md:ml-0',
-        )}>
-        <Header />
-        <main className={cn('flex-1 bg-custom-sidebar-bg px-[16px] py-[20px] md:px-[24px]')}>
-          <TaskList />
-        </main>
+          className={cn(
+            'flex-1 h-full',
+            'flex flex-col',
+            'transition-all duration-300',
+            isOpen ? 'md:ml-[300px]' : 'md:ml-0',
+          )}>
+          <Header />
+          <main className={cn('flex-1 bg-custom-sidebar-bg px-[16px] py-[20px] md:px-[24px]')}>
+            <TaskList />
+          </main>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }

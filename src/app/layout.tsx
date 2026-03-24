4ae10962028cc23cc16d9e82ad4cc29b7bffc2b1
carokann1945 <carokann1945@gmail.com>
2026-03-25 03:20:08 +0900

    chore: google, naver 서치 콘솔 코드 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,12 @@ export const metadata: Metadata = {
   title: 'Carokann',
   description:
     '탭별로 루틴과 반복 작업을 정리하고, 매일·매주·매월·매년 혹은 커스텀 주기에 맞춰 자동으로 다시 시작하는 개인용 작업 리셋 트래커.',
+  verification: {
+    google: 'KjPEoW3dDAV__bk3baW6Zb8lZW81wimVzu5tDiJL2Hc',
+    other: {
+      'naver-site-verification': '5587d60ce0f399389f76d05db48c693390fa770e',
+    },
+  },
 };
 
 export default function RootLayout({
